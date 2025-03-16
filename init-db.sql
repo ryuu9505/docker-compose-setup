88bdeccf74f3d78144c740f7539c9ac50494d80f
jun <ryuu.public@gmail.com>
2025-03-17 01:56:48 +0900

    feat: add initial query for creating a database

--- init-db.sql
+++ init-db.sql
@@ -1,9 +1,8 @@
 -- Create the database for Neubility Platform Core
 CREATE DATABASE platform_core;
+CREATE DATABASE platform_core_tenant;
 
--- Connect to the platform_core database
+-- Connect to the platform_core database and enable extensions
 \c platform_core;
-
--- Enable PostGIS and pgRouting extensions
 CREATE EXTENSION IF NOT EXISTS postgis;
-CREATE EXTENSION IF NOT EXISTS pgrouting; 
+CREATE EXTENSION IF NOT EXISTS pgrouting;
